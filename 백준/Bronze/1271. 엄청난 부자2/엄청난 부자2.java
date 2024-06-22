0952e387271565a6f2e0ad3eb33458ad824529fe
wijihooooon <dnlwlgns1117@naver.com>
2024-06-23 04:00:33 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 128 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,20 @@
+//BigInteger 연습
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		BigInteger a = new BigInteger(st.nextToken());
+		BigInteger b = new BigInteger(st.nextToken());
+		
+		System.out.println(a.divide(b));
+		System.out.println(a.remainder(b));
+	}
+}
