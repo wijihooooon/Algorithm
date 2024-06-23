68a7f3239bd617e66c55e28470547c4af50ee80b
wijihooooon <dnlwlgns1117@naver.com>
2024-06-23 16:48:54 +0900

    [Silver III] Title: N과 M (7), Time: 652 ms, Memory: 123092 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static int N, M;
+	
+	public static int arr[];
+	public static int num[];
+	public static boolean isused[];
+	
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N];
+		num = new int[N];
+		isused = new boolean[10001];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(num);
+		
+		func(0);
+		System.out.println(sb);
+	}
+
+	private static void func(int x) {
+		if(x == M) {
+			for(int i=0; i<M; i++) {
+				sb.append(num[arr[i]]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=0; i<N; i++) {
+			arr[x] = i;
+			func(x+1);
+		}
+	}
+}
