bd83ace86f4838793bb985e3cb1d5a241aa6cceb
wijihooooon <dnlwlgns1117@naver.com>
2024-06-23 16:49:29 +0900

    [Silver II] Title: N과 M (10), Time: 144 ms, Memory: 15864 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static int num[];
+	public static int arr[];
+	public static boolean isused[];
+	
+	public static int N, M;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		num = new int[N];
+		isused = new boolean[10001];
+		arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(num);
+		
+		func(0);
+		
+	}
+
+	private static void func(int x) {
+		if(x == M) {
+			for(int i=0; i<M; i++) {
+				System.out.print(num[arr[i]] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		int st = 0;
+		if(x != 0) {
+			st = arr[x-1] + 1;
+		}
+		
+		int temp = -1;
+		
+		for(int i=st; i<N; i++) {
+			if(!isused[i] && temp < num[i]) {
+				arr[x] = i;
+				isused[i] = true;
+				temp = num[i];
+				func(x+1);
+				isused[i] = false;
+			}
+		}
+	}
+
+}
