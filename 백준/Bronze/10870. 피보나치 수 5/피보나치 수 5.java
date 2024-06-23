fef27914f3cfee9ce2d3e23146bc852c8f1f5cd8
wijihooooon <dnlwlgns1117@naver.com>
2024-06-23 16:39:39 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int dp[] = new int[21];
+		
+		dp[0] = 0;
+		dp[1] = 1;
+		
+		for(int j=2; j<=N; j++) {
+			dp[j] = dp[j-1] + dp[j-2];
+		}
+		System.out.println(dp[N]);
+	}
+}
