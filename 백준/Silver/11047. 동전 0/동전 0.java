3f19da1d0ef0044e648162245c68a4b71799a3ae
wijihooooon <dnlwlgns1117@naver.com>
2024-06-25 09:31:15 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int ans = 0;
+		int arr[] = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		for(int i=N-1; i>=0; i--) {
+			ans += K/arr[i];
+			K %= arr[i];
+		}
+		
+		System.out.println(ans);
+	}
+
+}
