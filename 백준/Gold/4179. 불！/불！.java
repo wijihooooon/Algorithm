bbf7e4778b30142f4cb0d50e026619d6902e0e58
wijihooooon <dnlwlgns1117@naver.com>
2024-07-02 22:06:41 +0900

    [Gold III] Title: 불!, Time: 528 ms, Memory: 73244 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -6,16 +6,16 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static int N,M;
+
+	public static int N, M;
 	
-	public static int fire_Map[][];
-	public static int JH_Map[][];
-	
-	public static Queue<int[]> fire_Q = new LinkedList<>();
-	public static Queue<int[]> JH_Q = new LinkedList<>();
+	public static int map[][];
 	
 	public static int dx[] = {-1, 1, 0, 0};
 	public static int dy[] = {0, 0, -1, 1};
+	
+	public static Queue<int[]> Jq = new LinkedList<>();
+	public static Queue<int[]> Fq = new LinkedList<>();
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,76 +24,86 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		fire_Map = new int[N][M];
-		JH_Map = new int[N][M];
+		map = new int[N][M];
 		
-		for(int j=0; j<N; j++) { // 삽입~
+		for(int i=0; i<N; i++) {
 			String s = br.readLine();
-			for(int k=0; k<M; k++) {
-				if(s.charAt(k) == '#') {
-					fire_Map[j][k] = -1;
-					JH_Map[j][k] = -1;
-				}else if(s.charAt(k) == 'J') {
-					JH_Q.add(new int[] {j,k});
-					JH_Map[j][k] = 1;
-				}else if(s.charAt(k) == 'F') {
-					fire_Q.add(new int[] {j,k});
-					fire_Map[j][k] = 1;
+			for(int j=0; j<M; j++) {
+				if(s.charAt(j) == '#') {
+					map[i][j] = -3;
+				}else if(s.charAt(j) == '.') {
+					map[i][j] = -1;
+				}else if(s.charAt(j) == 'J') {
+					map[i][j] = 0;
+					Jq.add(new int[] {i, j});
+				}else if(s.charAt(j) == 'F') {
+					map[i][j] = 0;
+					Fq.add(new int[] {i, j});
 				}
 			}
 		}
 		
-		int x, y;
-		
-		// bfs 불 시작~
-			
-		while(!fire_Q.isEmpty()) {
-			x = fire_Q.peek()[0];
-			y = fire_Q.peek()[1];
-			fire_Q.poll();
-			
-			for(int j=0; j<4; j++) {
-				int cx = x + dx[j];
-				int cy = y + dy[j];
-				
-				if(cx>=0 && cy>=0 && cx<N && cy<M) {
-					if(fire_Map[cx][cy] == 0) {
-						fire_Map[cx][cy] = fire_Map[x][y] + 1;
-						fire_Q.add(new int[] {cx, cy});
-					}
-				}
-			}
+		if(Jq.peek()[0] == 0 || Jq.peek()[1] == 0 || Jq.peek()[0] == N-1 || Jq.peek()[1] == M-1) {
+			System.out.println(1);
+			return;
 		}
 		
-		// bfs 지훈이 시작~
+		fire_bfs();
 		
-		while(!JH_Q.isEmpty()) {
-			x = JH_Q.peek()[0];
-			y = JH_Q.peek()[1];
-			JH_Q.poll();
-			
-			if(x == 0 || x == N-1 || y == 0 || y == M-1) { // 이미 탈출 지점에 있는경우
-				System.out.println(JH_Map[x][y]);
-				return;
-			}
-			
-			for(int j=0; j<4; j++) {
-				int cx = x + dx[j];
-				int cy = y + dy[j];
-				
-				if(cx>=0 && cy>=0 && cx<N && cy<M) {
-					if(JH_Map[cx][cy] == 0 && (JH_Map[x][y]+1 < fire_Map[cx][cy] || fire_Map[cx][cy] == 0)) { // || fire_Map[cx][cy] == 0 불이 안간 곳
-						JH_Map[cx][cy] = JH_Map[x][y] + 1;
-						JH_Q.add(new int[] {cx, cy});
-						
-						if(cx == 0 || cx == N-1 || cy == 0 || cy == M-1) {
-							System.out.println(JH_Map[cx][cy]);
-							return;
-						}
-					}
-				}
-			}
-		}	
-		System.out.println("IMPOSSIBLE ");	
+
+		int ans = J_bfs();
+	
+		if(ans == -1) { // 탈출 지점에서 시작하는경우
+			System.out.println("IMPOSSIBLE");
+		}else {
+			System.out.println(ans);
+		}
+		
 	}
+	
+	private static void fire_bfs() {
+		
+		while(!Fq.isEmpty()) {
+			int x = Fq.peek()[0];
+			int y = Fq.peek()[1];
+			Fq.poll();
+			
+			for(int i=0; i<4; i++) {
+				int cx = x + dx[i];
+				int cy = y + dy[i];
+				
+				if(cx>=0 & cy>=0 && cx<N && cy<M) {
+					if(map[cx][cy] == -1) {
+						map[cx][cy] = map[x][y] + 1;
+						Fq.add(new int[] {cx, cy});
+					}
+				}
+			}
+		}
+	}
+	
+	private static int J_bfs() {
+		while(!Jq.isEmpty()) {
+			int x = Jq.peek()[0];
+			int y = Jq.peek()[1];
+			Jq.poll();
+			
+			for(int i=0; i<4; i++) {
+				int cx = x + dx[i];
+				int cy = y + dy[i];
+				
+				if(cx>=0 & cy>=0 && cx<N && cy<M) {
+					if(map[cx][cy] > map[x][y] + 1 || map[cx][cy] == -1) { // 불이 없는경우 map[cx][cy] == -1
+						map[cx][cy] = map[x][y] + 1;
+						if(cx == 0 || cx == N-1 || cy == 0 || cy == M-1) {
+							return map[cx][cy] + 1;
+						}
+						Jq.add(new int[] {cx, cy});
+					}
+				}
+			}
+		}
+		return -1;
+	}
+
 }
