29c8599cf589e4eec559f9451db7ef9b809bb20d
wijihooooon <dnlwlgns1117@naver.com>
2024-07-06 01:52:40 +0900

    [Gold IV] Title: 연구소, Time: 980 ms, Memory: 302716 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -0,0 +1,116 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static int N, M, virus, space;
+	public static int wall = 3;
+	public static int max = 0;
+	
+	public static int map[][];
+	public static int copyMap[][];
+	
+	public static ArrayList<int[]> virusList = new ArrayList<>();
+	public static boolean visited[][];
+	public static int dx[] = {-1, 1, 0, 0};
+	public static int dy[] = {0, 0, -1, 1};
+	
+	public static Queue<int[]> q = new LinkedList<>(); // 바이러스용 q~
+	public static Queue<int[]>copyQ = new LinkedList<>();
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new int[N][M];
+		
+		visited = new boolean[N][M];
+		
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == 1) {
+					wall++;
+					space--;
+				}else if(map[i][j] == 2) {
+					virus++;
+					space--;
+					virusList.add(new int[] {i, j});
+				}
+			}
+		}
+		
+		
+		buildTheWall(0);
+		
+		System.out.println(max);
+		
+	}
+
+	private static void buildTheWall(int cnt) {
+		if(cnt == 3) {
+			bfs();
+			while(!copyQ.isEmpty()) {
+				int x = copyQ.peek()[0];
+				int y = copyQ.peek()[1];
+				copyQ.poll();
+				
+				map[x][y] = 0;
+			}
+			return;
+		}
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				if(map[i][j] == 0 && !visited[i][j]) {
+					visited[i][j] = true;
+					map[i][j] = 1;
+					buildTheWall(cnt+1);
+					visited[i][j] = false;
+					map[i][j] = 0;
+				}
+			}
+		}
+	}
+
+	private static void bfs() {
+		space = N*M - virus - wall;
+		
+		for(int i=0; i<virusList.size(); i++) {
+			q.add(new int[] {virusList.get(i)[0], virusList.get(i)[1]});
+		}
+		
+		int newVirus = 0;
+		
+		while(!q.isEmpty()) {
+			int x = q.peek()[0];
+			int y = q.peek()[1];
+			q.poll();
+			
+			for(int i=0; i<4; i++) {
+				int cx = x + dx[i];
+				int cy = y + dy[i];
+				
+				if(cx>=0 && cy>=0 && cx<N && cy<M) {
+					if(map[cx][cy] == 0) {
+						map[cx][cy] = 2;
+						q.add(new int[] {cx, cy});
+						copyQ.add(new int[] {cx, cy});
+						newVirus++;
+					}
+				}
+			}
+		}
+		max = Math.max(max, space-newVirus);
+	}
+
+}
