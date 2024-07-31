ec0ad73b270b6bb1839d030d2d251daeb9ebb370
wijihooooon <dnlwlgns1117@naver.com>
2024-07-31 23:02:04 +0900

    [Silver II] Title: 전구 길만 걷자, Time: 308 ms, Memory: 15064 KB -BaekjoonHub

---
+++ 백준/Silver/17359. 전구 길만 걷자/전구 길만 걷자.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/*
+ * 그냥 백트레킹으로 풀면 시간초가가 나서 각 전구 묶음마다 몇번씩 변하는지 구하고
+ * 백트래킹하면서 연결되는 부분의 변화 유무만 더해 최소값 추출
+ * */
+public class Main {
+
+	public static int N, min, ans;
+	public static String light[];
+	public static boolean visited[];
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		ans = 0;
+		min = Integer.MAX_VALUE;
+		
+		light = new String[N];
+		visited = new boolean[N];
+		
+		for(int i=0; i<N; i++) {
+			light[i] = br.readLine();
+			// 각 전구 묶음내에서 01/10변하는거 찾아내기
+			innerChange(light[i]);
+		}
+
+		backTracking(0, -1, 0);
+		
+		System.out.println(ans + min);
+	}
+	// 각 전구 묶음내에서 변화 수 세기
+	private static void innerChange(String str) {
+		for(int i=0; i<str.length()-1; i++) {
+			if(str.charAt(i) != str.charAt(i+1)) {
+				ans++;
+			}
+		}
+	}
+
+	// 백트래킹으로 조합 만들면서 연결되는 부분 바뀌는거 세기
+	private static void backTracking(int cnt, int lastIndex, int count) {
+		if(min <= count) {
+			return;
+		}
+		// cnt가 N이되면 N개로 만들수 있는 조합이 구해짐
+		if(cnt == N) {
+			min = Math.min(min, count);
+			return;
+		}		
+		
+		for(int i=0; i<N; i++) {
+			if(!visited[i]) {
+				
+				visited[i] = true;
+				
+				int newCount = count;
+				
+				if(lastIndex != -1 && light[lastIndex].charAt(light[lastIndex].length() - 1) != light[i].charAt(0)) {
+					newCount++;
+				}
+				
+				backTracking(cnt+1,i, newCount);
+				
+				visited[i] = false;
+			}
+		}
+	}
+
+}
