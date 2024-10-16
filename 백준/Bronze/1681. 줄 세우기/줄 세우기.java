9a27e8242c7592f88a8ceff3147aeda9c13556da
wijihooooon <dnlwlgns1117@naver.com>
2024-10-17 01:28:10 +0900

    [Bronze II] Title: 줄 세우기, Time: 268 ms, Memory: 113036 KB -BaekjoonHub

---
+++ 백준/Bronze/1681. 줄 세우기/줄 세우기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int ans = 0;
+		int cnt = 0;
+		int num = 1;
+		
+		while(cnt != N) {
+            
+			if(!Integer.toString(num).contains(Integer.toString(M))) {
+				ans = num;
+				cnt++;
+			}
+			num++;
+		}
+		System.out.println(ans);
+	}
+}
