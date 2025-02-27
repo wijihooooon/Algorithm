db97ab05bc8d2679abf665bb483348bcd44e7bea
wijihooooon <dnlwlgns1117@naver.com>
2025-02-27 14:57:45 +0900

    [D4] Title: 정사각형 방, Time: 158 ms, Memory: 39,376 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -1,82 +1,85 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
+    static int n,m;
+    static int[][] arr;
+    static boolean[] visited;
+    static Point[] nums;
 
-	public static int N;
-	
-	public static int[][] map;
-	
-	public static int[] dx = {-1, 1, 0, 0};
-	public static int[] dy = {0, 0, -1, 1};
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		int count;
-		int num;
-		
-		for(int t=1; t<=T; t++) {
-			N = Integer.parseInt(br.readLine());
-			
-			count = 0;
-			num = 0;
-			
-			map = new int[N][N];
-			
-			for(int i=0; i<N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for(int j=0; j<N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
-					int newCount = bfs(i, j);
-					
-					if((count < newCount) || (count == newCount && num > map[i][j])) {
-						count = newCount;
-						num = map[i][j];
-					}
-				}
-			}
-			
-			
-			sb.append("#").append(t).append(" ").append(num).append(" ").append(count).append("\n");
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
 
-	private static int bfs(int x, int y) {
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {x, y});
-		int cnt = 1;
-		
-		while(!q.isEmpty()) {
-			x = q.peek()[0];
-			y = q.peek()[1];
-			q.poll();
-			
-			for(int i=0; i<4; i++) {
-				int cx = x + dx[i];
-				int cy = y + dy[i];
-				
-				if(cx>=0 && cy>=0 && cx<N && cy<N) {
-					if(map[cx][cy] == map[x][y] + 1) {
-						q.add(new int[] {cx, cy});
-						cnt++;
-					}
-				}
-			}
-		}
-		return cnt;
-	}
+        int tc = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= tc; t++) {
+            n = Integer.parseInt(br.readLine());
+            arr = new int[n][n];
+            m=n*n;
+            visited=new boolean[m+1];
+
+
+            // 숫자 i의 위치를 담는 배열
+            // ex) 숫자 1이 (2,3) 에 존재한다면 nums[1]=(2,3) 이 들어감
+            Point[] nums=new Point[m+1];
+
+
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++){
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                    nums[arr[i][j]] = new Point(i, j);
+                }
+            }
+
+
+            int answer=0,max=0;
+            for (int i = 1; i <=m; i++) {
+                if(visited[i])
+                    continue;
+                int cnt = move_bfs(nums[i]);
+                if (cnt > max) {
+                    answer= i;
+                    max=cnt;
+                }
+            }
+            sb.append("#").append(t).append(" ").append(answer)
+                    .append(" ").append(max).append("\n");
+        }
+        System.out.println(sb);
+    }
+    static int move_bfs(Point start) {
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0 ,1, -1};
+        int cnt=0;
+        Queue<Point> q = new ArrayDeque<>();
+        q.add(start);
+
+        while (!q.isEmpty()) {
+            Point cur = q.poll();
+            cnt++;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+                if(nx<0 || nx>=n || ny<0 || ny>=n || arr[nx][ny]!=arr[cur.x][cur.y]+1)
+                    continue;
+                q.add(new Point(nx, ny));
+                visited[arr[nx][ny]]=true;
+            }
+        }
+        return cnt;
+    }
+    
+    
+}
+
+class Point{
+    int x,y;
+
+    public Point(int x, int y) {
+        this.x=x;
+        this.y=y;
+    }
 }
