6f1a1c24b1d0d4608a33fa93e26b2e40dce771a1
wijihooooon <dnlwlgns1117@naver.com>
2025-03-04 23:37:15 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 668 ms, Memory: 131096 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,46 +1,44 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		int dp[][] = new int[N+1][N+1];
+		int map[][] = new int[N+1][N+1];
 		
-		for(int j=1; j<=N; j++) {
+		for(int i=1; i<=N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int k=1; k<=N; k++) {
-				dp[j][k] = dp[j][k-1] + Integer.parseInt(st.nextToken());
+			for(int j=1; j<=N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		for(int j=0; j<M; j++) {
+		int[][] sum = new int[N+1][N+1];
+		
+		for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                sum[i][j] = map[i][j] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+            }
+        }
+		
+		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			
-			int x1 = Integer.parseInt(st.nextToken());
-			int y1 = Integer.parseInt(st.nextToken());
-			int x2 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
-			
-			int ans = 0;
-			
-			for(int k=x1; k<=x2; k++) {
-				ans += dp[k][y2] - dp[k][y1-1];
-			}
-			sb.append(ans).append("\n");
-		}
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+			sb.append(sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1]).append("\n");
+		}		
 		System.out.println(sb);
 	}
-
 }
