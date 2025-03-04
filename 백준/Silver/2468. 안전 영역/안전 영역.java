e2d8a145223f105ade3cf7f8444de0d2e6f27d85
wijihooooon <dnlwlgns1117@naver.com>
2025-03-04 15:55:13 +0900

    [Silver I] Title: 안전 영역, Time: 232 ms, Memory: 52712 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,65 +1,76 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Main{
-	public static int grid[][];
-	public static boolean visited[][];
-	public static int dirX[] = {0, 0, -1, 1}; // 상 하 좌 우
-	public static int dirY[] = {1, -1, 0, 0} ;
+public class Main {
+
 	public static int N;
-	
-	public static void main(String args[]) throws IOException {
+	public static int[][] map;
+	public static boolean[][] visited;
+	public static int[] dx = {-1, 1, 0, 0};
+	public static int[] dy = {0, 0, -1, 1};
+	public static Queue<int[]> q;
+	public static void main(String[] args)throws IOException{
+		//--------------솔루션 코드를 작성하세요.--------------------------------
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
 		
 		N = Integer.parseInt(br.readLine());
+		int max = 0;
+		int min = Integer.MAX_VALUE;
 		
-		grid = new int[N+1][N+1];
+		map = new int[N][N];
 		
-		int maxHeight = 0;
-		for(int i=1; i<=N; i++) {	// 격자판에 침수지역 삽입
+		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<=N; j++) {
-				grid[i][j] = Integer.parseInt(st.nextToken());
-				if(grid[i][j] >maxHeight) {
-					maxHeight = grid[i][j];
-				}
+			for(int j=0; j<N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				max = Math.max(max, map[i][j]);
+				min = Math.min(min, map[i][j]);
 			}
 		}
 		
-		int max = 0;
-		for(int height=0; height<=maxHeight; height++) {
-			visited = new boolean[N+1][N+1];
-			int count = 0;
-			for(int y=1; y<=N; y++) {
-				for(int x=1; x<=N; x++) {
-					if(!visited[y][x] && grid[y][x] > height) {
-						count += DFS(x, y, height); 
+		int count = 0;
+		int ans = 1;
+		
+		for(int i=min-1; i<=max; i++) {
+			visited = new boolean[N][N];
+			count = 0;
+			q = new LinkedList<>();
+			for(int j=0; j<N; j++) {
+				for(int k=0; k<N; k++) {
+					if(!visited[j][k] && map[j][k] > i) {
+						q.add(new int[] {j, k});
+						visited[j][k] = true;
+						bfs(i);
+						count++;
 					}
 				}
 			}
-			max = Math.max(max, count);
+			ans = Math.max(count, ans);
 		}
-		
-		sb.append(max);
-		System.out.println(sb);		
-		
+		System.out.println(ans);
 	}
-	private static int DFS(int x, int y, int height) {
-		visited[y][x] = true;
-		for(int i=0; i<4; i++) {
-			int nx = x + dirX[i];
-			int ny = y + dirY[i];
+	private static void bfs(int flower) {
+		while(!q.isEmpty()) {
+			int x = q.peek()[0];
+			int y = q.peek()[1];
+			q.poll();
 			
-			if(nx>N || ny>N || nx<1 || ny<1) continue;
-			if(visited[ny][nx]) continue;
-			if(grid[ny][nx] > height) {
-				DFS(nx, ny, height);
+			for(int i=0; i<4; i++) {
+				int cx = x + dx[i];
+				int cy = y + dy[i];
+				if(cx>=0 && cy>=0 && cx<N && cy<N) {
+					if(!visited[cx][cy] && map[cx][cy] > flower) {
+						q.add(new int[] {cx, cy});
+						visited[cx][cy] = true;
+					}
+				}
 			}
 		}
-		return 1;
 	}
+
 }
