c34b759adffbf1d659f34376033be720d8edd570
wijihooooon <dnlwlgns1117@naver.com>
2025-03-09 20:47:20 +0900

    [Silver III] Title: N과 M (1), Time: 1624 ms, Memory: 61836 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -4,40 +4,37 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
 	public static int N, M;
-	public static int arr[];
-	public static boolean isused[];
-	
-	public static void main(String[] args) throws IOException{
+	public static boolean[] visited;
+	public static int[] temp;
+	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		arr = new int[10];
-		isused = new boolean[10];
-		func(0);
+		visited = new boolean[N+1];
+		temp = new int[M];
+		
+		dfs(0);
 	}
 
-	private static void func(int a) {
-		if(a==M) {
+	private static void dfs(int cnt) {
+		if(cnt == M) {
 			for(int i=0; i<M; i++) {
-				System.out.print(arr[i] + " ");
+				System.out.print(temp[i] + " ");
 			}
 			System.out.println();
 			return;
 		}
-		
+				
 		for(int i=1; i<=N; i++) {
-			if(!isused[i]) {
-				arr[a] = i;
-				isused[i] = true;
-				func(a + 1);
-				isused[i] = false;
-			}
-			
+			if(visited[i]) continue;
+			visited[i] = true;
+			temp[cnt] = i;
+			dfs(cnt+1);
+			visited[i] = false;
 		}
 	}
 }
