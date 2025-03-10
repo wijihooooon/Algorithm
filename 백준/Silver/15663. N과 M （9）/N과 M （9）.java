6478aa3538347d419f23c9f6785a11eddf13b86d
wijihooooon <dnlwlgns1117@naver.com>
2025-03-10 09:42:23 +0900

    [Silver II] Title: N과 M (9), Time: 112 ms, Memory: 18044 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -6,52 +6,53 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	public static int N, M;
-	
-	public static int arr[];
-	public static boolean isused[];
-	public static int num[];
-	public int check = Integer.MIN_VALUE;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		isused = new boolean[10001];
-		arr = new int[10];
-		num = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			num[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(num);
-		
-		func(0);
-	}
+    public static int N, M;
 
-	private static void func(int x) {
-		if(x == M) {
-			for(int i=0; i<M; i++) {
-				System.out.print(num[arr[i]] + " ");
-			}
-			System.out.println();
-			return;
-		}
-		
-		int temp = -1;
-		
-		for(int i=0; i<N; i++) {
-			if(!isused[i] && num[i] != temp) {
-				arr[x] = i;
-				isused[i] = true;
-				temp = num[arr[x]];
-				func(x+1);
-				isused[i] = false;
-			}
-		}
-	}
+    public static int[] arr;
+    public static int[] temp;
+    public static boolean[] visited;
+
+    public static StringBuilder sb;
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        temp = new int[M];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int cnt) {
+        if(cnt == M){
+            for(int i=0; i<M; i++){
+                sb.append(temp[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int check = -1;
+
+        for(int i=0; i<N; i++){
+            if(visited[i] || arr[i] == check) continue;
+            visited[i] = true;
+            temp[cnt] = arr[i];
+            check = arr[i];
+            dfs(cnt+1);
+            visited[i] = false;
+        }
+    }
 }
