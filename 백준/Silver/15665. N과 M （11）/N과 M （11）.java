811eb2e67a94e245edaa4725e5c352af37240e1e
wijihooooon <dnlwlgns1117@naver.com>
2025-03-10 10:18:36 +0900

    [Silver II] Title: N과 M (11), Time: 272 ms, Memory: 45312 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.java
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -6,54 +6,49 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	public static int N, M;
-	
-	public static int num[];
-	public static int arr[];
-	public static boolean isused[];
-	public static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		num = new int[N];
-		arr = new int[N];
-		isused = new boolean[10001];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			num[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(num);
-		
-		func(0);
-        
-        System.out.println(sb);
-        
-	}
+    public static int N, M;
 
-	private static void func(int x) {
-		if(x == M) {
-			for(int i=0; i<M; i++) {
-				sb.append(num[arr[i]]).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		int temp = -1;
-		
-		for(int i=0; i<N; i++) {
-			if(temp != num[i]) {
-				arr[x] = i;
-				temp = num[i];
-				func(x+1);
-			}
-		}
-	}
+    public static int[] arr;
+    public static int[] temp;
+
+    public static StringBuilder sb;
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        temp = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.println(sb);
+    }
+    public static void dfs(int cnt) {
+        if(cnt == M) {
+            for(int i=0; i<M; i++){
+                sb.append(temp[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int check = -1;
+
+        for(int i=0; i<N; i++){
+            if(arr[i] == check) continue;
+            temp[cnt] = arr[i];
+            check = arr[i];
+            dfs(cnt+1);
+        }
+    }
 }
