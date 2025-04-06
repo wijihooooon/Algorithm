7c18a15e9a8278dd06522bd95cda8a903eac6d30
wijihooooon <dnlwlgns1117@naver.com>
2025-04-07 01:07:32 +0900

    [Silver IV] Title: 수 찾기, Time: 1268 ms, Memory: 67540 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -23,27 +23,11 @@ public class Main {
 		int M = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<M; i++) {
-			binarySearch(arr, Integer.parseInt(st.nextToken()));
-		}
-	}
-
-	private static void binarySearch(int[] arr, int target) {
-		int start = 0;
-		int end = arr.length-1;
-		
-		while(start<=end) {
-			int mid = (start + end) / 2;
-			
-			if(arr[mid] == target){
+			if(Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0) {
 				System.out.println(1);
-				return;
-			}else if(arr[mid] < target) {
-				start = mid + 1;
 			}else {
-				end = mid - 1;
+				System.out.println(0);
 			}
 		}
-		System.out.println(0);
-		return;
 	}
 }
