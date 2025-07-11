099201c71f4cd92b615772ba569199b62b9ec842
wijihooooon <dnlwlgns1117@naver.com>
2025-07-11 12:02:25 +0900

    [Gold IV] Title: 테트로미노, Time: 460 ms, Memory: 30444 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,102 +1,85 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+
+    public static int N, M;
+    public static int[][] map;
+
+    public static int[][][] type = {
+            {{0, 0}, {0, 1}, {0, 2}, {0, 3}}, // ㅡ
+            {{0, 0}, {1, 0}, {2, 0}, {3, 0}}, // ㅣ
+
+            {{0, 0}, {0, 1}, {1, 0}, {1, 1}}, // ㅁ
+
+            {{0,0},{1,0},{2,0},{2,1}},   // └
+            {{0,1},{1,1},{2,1},{2,0}},   // ┘
+            {{0,0},{0,1},{0,2},{1,0}},   // ┌
+            {{0,0},{0,1},{0,2},{1,2}},   // ┐
+            {{0,0},{1,0},{1,1},{1,2}},   // Γ (L 90°)
+            {{0,2},{1,0},{1,1},{1,2}},   // _| (L 270°)
+            {{0,0},{0,1},{1,0},{2,0}},   // ⎡ (L 180°)
+            {{0,0},{0,1},{1,1},{2,1}},   // ⎣ (L 180° 대칭)  
+
+            // Z (S) 계열 (4개)
+            {{0,0},{0,1},{1,1},{1,2}},   // S 가로  
+            {{1,0},{1,1},{0,1},{0,2}},   // Z 가로  
+            {{0,1},{1,1},{1,0},{2,0}},   // S 세로  
+            {{0,0},{1,0},{1,1},{2,1}},   // Z 세로  
+        
+            // T (ㅜ) 계열 (4개)  
+            {{0,0},{0,1},{0,2},{1,1}},   // ㅜ  
+            {{0,1},{1,0},{1,1},{2,1}},   // ㅏ  
+            {{1,0},{1,1},{1,2},{0,1}},   // ㅗ  
+            {{0,0},{1,0},{2,0},{1,1}}    // ㅓ  
+    };
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int[][][] map = new int[4][501][501];
+        int ans = 0;
 
-        // 값 삽입
+        map = new int[N][M];
+
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<M; j++){
-                map[0][i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        Queue<Integer> q;
-        int x = N;
-        int y = M;
-        int temp = 0;
-        // map 회전
-        for(int i=1; i<4; i++){
-            q = new ArrayDeque<>();
-            for(int j=0; j<x; j++){
-                for(int k=0; k<y; k++){
-                    q.add(map[i-1][j][k]);
-                }
-            }
-            temp = x;
-            x = y;
-            y = temp;
-            for(int j=y-1; j>=0; j--){
-                for(int k=0; k<x; k++){
-                    map[i][k][j] = q.poll();
-                }
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                ans = Integer.max(cal(i, j), ans);
             }
         }
 
-        int max = Integer.MIN_VALUE;
-        int sum1 = 0;
-        int sum2 = 0;
+        System.out.println(ans);
 
-        // 테트로미노 전부 비교
-        for(int i=0; i<4; i++){
-            if(i%2==0){
-                x = N;
-                y = M;
-            }else{
-                x = M;
-                y = N;
+    }
+
+    public static int cal(int x, int y){
+        int sum;
+        int flag;
+        int max = 0;
+        for(int i=0; i<type.length; i++){
+            sum = 0;
+            flag = 0;
+            for(int j=0; j<4; j++){
+                if(x + type[i][j][0] < 0 || x + type[i][j][0] >= N || y + type[i][j][1] < 0 || y + type[i][j][1] >= M) break;
+                sum += map[x + type[i][j][0]][y + type[i][j][1]];
+                flag++;
             }
-            // ㅡ
-            for(int j=0; j<x; j++){
-                for(int k=0; k<=y-4; k++){
-                    sum1 = map[i][j][k] + map[i][j][k+1] + map[i][j][k+2] + map[i][j][k+3];
-                    max = Math.max(max, sum1);
-                }
-            }
-            // L
-            for(int j=0; j<=x-3; j++){
-                for(int k=0; k<=y-2; k++){
-                    sum1 = map[i][j][k] + map[i][j+1][k] + map[i][j+2][k] + map[i][j+2][k+1];
-                    sum2 = map[i][j][k+1] + map[i][j+1][k+1] + map[i][j+2][k] + map[i][j+2][k+1];
-                    max = Math.max(max, Math.max(sum1, sum2));
-                }
-            }
-            // N
-            for(int j=0; j<=x-3; j++){
-                for(int k=0; k<=y-2; k++){
-                    sum1 = map[i][j][k] + map[i][j+1][k] + map[i][j+1][k+1] + map[i][j+2][k+1];
-                    sum2 = map[i][j][k+1] + map[i][j+1][k] + map[i][j+1][k+1] + map[i][j+2][k];
-                    max = Math.max(max, Math.max(sum1, sum2));
-                }
-            }
-            // ㅜ
-            for(int j=0; j<=x-2; j++){
-                for(int k=0; k<=y-3; k++){
-                    sum1 = map[i][j][k] + map[i][j][k+1] + map[i][j][k+2] + map[i][j+1][k+1];
-                    sum2 = map[i][j+1][k] + map[i][j+1][k+1] + map[i][j+1][k+2] + map[i][j][k+2];
-                    max = Math.max(max, Math.max(sum1, sum2));
-                }
-            }
-            // ㅁ
-            for(int j=0; j<=x-2; j++){
-                for(int k=0; k<=y-2; k++){
-                    sum1 = map[i][j][k] + map[i][j+1][k] + map[i][j][k+1] + map[i][j+1][k+1];
-                    max = Math.max(max, sum1);
-                }
+            if(flag == 4){
+                max = Integer.max(sum, max);
             }
         }
-        System.out.println(max);
+        return max;
     }
 }
