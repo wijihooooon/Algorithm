e732b89bd60de74bd8d4b039b2c938de421d2adf
wijihooooon <dnlwlgns1117@naver.com>
2026-01-14 06:34:19 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 316 ms, Memory: 39408 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -1,49 +1,43 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-    public static void main(String args[])throws IOException{
+public class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
 
-        for(int t=0; t<T; t++){
+        while (T-- > 0) {
             st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
 
-            int A[] = new int[N];
-            int B[] = new int[M];
+            int[] A = new int[N];
+            int[] B = new int[M];
 
             st = new StringTokenizer(br.readLine());
-            for(int i=0; i<N; i++){
-                A[i] = Integer.parseInt(st.nextToken());
-            }
+            for (int i = 0; i < N; i++) A[i] = Integer.parseInt(st.nextToken());
 
             st = new StringTokenizer(br.readLine());
-            for(int i=0; i<M; i++){
-                B[i] = Integer.parseInt(st.nextToken());
-            }
+            for (int i = 0; i < M; i++) B[i] = Integer.parseInt(st.nextToken());
 
             Arrays.sort(A);
             Arrays.sort(B);
 
-            int ans = 0;
-            
-            for(int i=0; i<N; i++){
-                int a = A[i];
-                for(int j=0; j<M; j++){
-                    if(a <= B[j]){
-                        break;
-                    }
-                    ans++;
+            long ans = 0; 
+            int j = 0;
+
+            for (int i = 0; i < N; i++) {
+                while (j < M && B[j] < A[i]) {
+                    j++;
                 }
+                ans += j;
             }
 
-            sb.append(ans).append("\n");
+            sb.append(ans).append('\n');
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
