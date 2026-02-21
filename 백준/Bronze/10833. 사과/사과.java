f0d364da7c82ff6fb2c2f4df914b00276b167978
wijihooooon <dnlwlgns1117@naver.com>
2026-02-22 02:56:16 +0900

    [Bronze III] Title: 사과, Time: 68 ms, Memory: 11596 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+        int ans = 0;
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            ans += b % a;
+        }
+        System.out.println(ans);
+    }
+}
