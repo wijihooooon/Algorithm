879c7adf91cd8de023f58870ea3b1babc1d56497
wijihooooon <dnlwlgns1117@naver.com>
2026-03-29 18:04:40 +0900

    [level 2] Title: 땅따먹기, Time: 37.98 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        
+        int dp[][] = new int[land.length][4];
+        
+        for(int i=0; i<4; i++){
+            dp[0][i] = land[0][i];
+        }
+        
+        for(int i=1; i<land.length; i++){
+            for(int j=0; j<4; j++){
+                int plusNum = 0;
+                for(int k=0; k<4; k++){
+                    if(k == j) continue;
+                    plusNum = Math.max(plusNum, dp[i-1][k]);
+                }
+                dp[i][j] = land[i][j] + plusNum;
+            }
+        }
+        
+        
+        for(int i=0; i<4; i++){
+            answer = Math.max(answer, dp[land.length-1][i]);
+        }
+        
+        return answer;
+    }
+}
