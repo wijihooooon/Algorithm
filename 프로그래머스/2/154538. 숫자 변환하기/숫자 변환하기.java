898622e5545397fa7928753e1309f19d5b4029d8
wijihooooon <dnlwlgns1117@naver.com>
2026-03-30 16:38:27 +0900

    [level 2] Title: 숫자 변환하기, Time: 34.70 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int x, int y, int n) {
+        int answer = 0;
+        
+        int dp[] = new int[y+1];
+        Arrays.fill(dp, -1);
+        dp[x] = 0;
+        
+        for (int i = x; i <= y; i++) {
+            if (dp[i] == -1) continue;
+
+            if (i + n <= y) {
+                if (dp[i + n] == -1 || dp[i + n] > dp[i] + 1) {
+                    dp[i + n] = dp[i] + 1;
+                }
+            }
+
+            if (i * 2 <= y) {
+                if (dp[i * 2] == -1 || dp[i * 2] > dp[i] + 1) {
+                    dp[i * 2] = dp[i] + 1;
+                }
+            }
+
+            if (i * 3 <= y) {
+                if (dp[i * 3] == -1 || dp[i * 3] > dp[i] + 1) {
+                    dp[i * 3] = dp[i] + 1;
+                }
+            }
+        }
+        
+        answer = dp[y];
+        
+        return answer;
+    }
+}
