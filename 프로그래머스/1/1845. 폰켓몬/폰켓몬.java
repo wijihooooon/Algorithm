18f209bc0b6a10edd71cd11d46feedba75b40ec3
wijihooooon <dnlwlgns1117@naver.com>
2026-05-07 21:52:52 +0900

    [level 1] Title: 폰켓몬, Time: 4.49 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i=0; i<nums.length; i++){
+            set.add(nums[i]);
+        }
+        
+        if(nums.length / 2 <= set.size()){
+            return nums.length/2;
+        }
+        
+        return set.size();
+    }
+}
