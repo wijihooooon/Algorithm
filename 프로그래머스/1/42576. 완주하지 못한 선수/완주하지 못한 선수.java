c0f076b12f66807c468fd90fc96aee0becf4dd43
wijihooooon <dnlwlgns1117@naver.com>
2026-09-09 08:05:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 54.08 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -6,24 +6,24 @@ class Solution {
         HashMap<String, Integer> map = new HashMap<>();
         
         for(int i=0; i<participant.length; i++){
-            map.put(participant[i], map.getOrDefault(participant[i], 0)+1);
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
         }
         
         for(int i=0; i<completion.length; i++){
             String name = completion[i];
             
             if(!map.containsKey(name)) continue;
-            
-            if(map.get(name) > 1){
+               
+            if(map.get(name)>1){
                 map.put(name, map.get(name)-1);
             }else{
                 map.remove(name);
             }
         }
-        
+               
         for(String name : map.keySet()){
             return name;
-        }
+        }           
         
         return answer;
     }
