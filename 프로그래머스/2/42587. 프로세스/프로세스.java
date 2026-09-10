afd74d7d1e39c0bb215314e935308efabf07de61
wijihooooon <dnlwlgns1117@naver.com>
2026-09-10 17:24:50 +0900

    [level 2] Title: 프로세스, Time: 0.77 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -5,28 +5,30 @@ class Solution {
         int answer = 0;
         
         Deque<int[]> dq = new ArrayDeque<>();
+        
         for(int i=0; i<priorities.length; i++){
             dq.addLast(new int[] {i, priorities[i]});
         }
         
         while(!dq.isEmpty()){
             int[] cur = dq.pollFirst();
-            boolean check = false;
             
-            for(int[] doc : dq){
-                if(doc[1] > cur[1]){
-                    check = true;
+            boolean hasHigher = false;
+            
+            for(int[] process : dq){
+                if(cur[1] < process[1]){
+                    hasHigher = true;
                     break;
                 }
             }
             
-            if(check){
+            if(hasHigher){
                 dq.addLast(cur);
             }else{
                 answer++;
                 if(cur[0] == location){
-                    return answer;
-                }
+                return answer;
+            }
             }
         }
         return answer;
