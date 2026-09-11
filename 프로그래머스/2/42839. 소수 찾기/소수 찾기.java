ae0b58e6f90ebae6ff5c1109a74bd7972c19ef81
wijihooooon <dnlwlgns1117@naver.com>
2026-09-11 20:24:44 +0900

    [level 2] Title: 소수 찾기, Time: 11.22 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,41 +3,44 @@ import java.util.*;
 class Solution {
     
     public Set<Integer> set = new HashSet<>();
-    public boolean[] visited;
     
     public int solution(String numbers) {
+        boolean[] visited = new boolean[numbers.length()];
+        
+        comb(numbers, visited, "");
+        
         int answer = 0;
-        visited = new boolean[numbers.length()];
         
-        comb(numbers, 0, "");
-        
-        for(int i : set){
-            if(isPrime(i)) answer++;
+        for(int n : set){
+            if(isPrime(n)){
+                answer++;
+            }
         }
         
         return answer;
     }
     
-    public void comb(String numbers, int idx, String num){
-        if(num != ""){
-            set.add(Integer.parseInt(num));
-        }
-        
-        if(idx == numbers.length()){
-            return;
+    public void comb(String numbers, boolean[] visited, String current){
+        if (!current.equals("")) {
+            set.add(Integer.parseInt(current));
         }
         
         for(int i=0; i<numbers.length(); i++){
             if(visited[i]) continue;
             
             visited[i] = true;
-            comb(numbers, idx+1, num + numbers.charAt(i));
+            
+            comb(numbers, visited, current + numbers.charAt(i));
+            
             visited[i] = false;
         }
     }
     
     public boolean isPrime(int num){
-        if(num == 0 || num == 1) return false;
+        
+        if (num < 2) {
+            return false;
+        }
         
         for(int i=2; i*i<=num; i++){
             if(num % i == 0) return false;
